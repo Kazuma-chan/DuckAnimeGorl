1e575b59196188403f1dc6377fb9e28c43a8a6e0
Kazuma <matheushenriquecss@gmail.com>
2021-03-22 18:07:48 -0300

    Start of JSfication

---
+++ style_injector.js
@@ -0,0 +1,7 @@
+let logo_homepage = document.getElementsByClassName('.logo_homepage');
+let darkBg_logo_homepage = document.getElementsByClassName('.dark-bg .logo_homepage');
+let darkHeader_header__logo = document.getElementsByClassName('.dark-header .header__logo');
+
+logo_homepage.style.background = "currentcolor url('chrome-extension://__MSG_@@extension_id__/logo_homepage.normal.v108.svg')";
+darkBg_logo_homepage.style.background = "currentcolor url('chrome-extension://__MSG_@@extension_id__/logo_homepage.alt.v108.svg')";
+darkHeader_header__logo.style.background = "currentcolor url('chrome-extension://__MSG_@@extension_id__/logo_header.alt.v108.svg')";
